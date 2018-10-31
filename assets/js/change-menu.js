8bab80c240d913644ee1819bb1f69d5f421de139
Rafael Nai <ramiresnai@gmail.com>
2018-10-30 23:34:10 -0300

    Responsive layout

--- assets/js/change-menu.js
+++ assets/js/change-menu.js
@@ -1,11 +1,20 @@
 const sectionSkills = document.querySelector('.home__content--btn')
-const changePoint = sectionSkills.getBoundingClientRect().top;
-const menu = document.querySelector('.menu');
+const changePoint = sectionSkills.getBoundingClientRect().top
+const menu = document.querySelector('.menu')
+const toggleMenu = document.querySelector('.toggle-menu')
 
-window.addEventListener('scroll', () => {
-  setInterval(() => {
-    window.scrollY > changePoint
-    ? menu.classList.add('menu-active')
-    : menu.classList.remove('menu-active');
-  }, 500)
-})
+if(window.innerWidth > 780) {
+  
+  window.addEventListener('scroll', () => {
+    setInterval(() => {
+      window.scrollY > changePoint ? menu.classList.add('menu-active') : menu.classList.remove('menu-active');
+    }, 500)
+  })
+} else {
+
+  toggleMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu-active');
+  })
+}
+
+
